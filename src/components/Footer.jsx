f6aaaade143cac5e4dad237ee4236b39d0387381
OJPalenzuela <palenzuelaomar@gmail.com>
2022-03-18 10:59:01 -0500

    Commit desde VS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <ul>
+        <Items>A</Items>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </FooterWrapper>
+  );
+};
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: #5b5b5b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Items = styled.li`
+  padding: 4px;
+  background-color: white;
+`;
+
+export default Footer;
